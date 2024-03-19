860b83d8bec00ffdebfd18b80809a7b585d22ef9
QIAF <qonitaafifah3@gmail.com>
2024-03-19 08:41:23 +0700

    live coding-nodejs-express-view-engine-main

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false})); // karna  pake html di client, kalo gaada ini nnti undefined
+app.use(express.static(path.join(__dirname, 'public'))); // agar gausah manggil filder kyak public dll
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
